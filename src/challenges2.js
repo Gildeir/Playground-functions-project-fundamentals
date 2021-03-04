bbb9ff0d53b9b67df9912ec1bcd6c476ff29a7d8
Gildeir <gildeirlopes@gmail.com>
2021-03-04 01:33:14 -0300

    refatorando e incrementando statement if no desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,23 @@
 // Desafio 10
 
-function techList(nameTech, name ) {
+
+function techList(NomeTech, name ) {
   
-  let arr = [];
+    let sortName = NomeTech.sort();
+    let arrInserted = [];
 
- 
-  for(index = 0; index < nameTech.length; index += 1){
+  if ((sortName.length == 0)||(sortName[0]=='')||(sortName[0]==" ")) {
+      return "Vazio!"}
 
-    arr[index] ={ tech: nameTech[index], name: name, };
-   
-  } return arr;
+  for(index = 0; index < sortName.length; index += 1){
+    let objectInserted = {};
+    objectInserted ["tech"] = sortName[index];    
+    objectInserted["name"] = name;
+    arrInserted.push(objectInserted);
+    
+  }return arrInserted;
 
-
-
-
-  
-}console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Lucas"))
+ }console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Lucas"))
 
 // Desafio 11
 function generatePhoneNumber() {
