db3e5b789671a937e7702f89c2da4abf128c636c
Gildeir <gildeirlopes@gmail.com>
2021-03-04 00:16:18 -0300

    Primeiras tentativas desafio 10 TechName

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+
+function techList(nameTech, name ) {
+  
+  let arr = [];
+
+ 
+  for(index = 0; index < nameTech.length; index += 1){
+
+    arr[index] ={ tech: nameTech[index], name: name, };
+   
+  } return arr;
+
+
+
+
+  
+}console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Lucas"))
 
 // Desafio 11
 function generatePhoneNumber() {
