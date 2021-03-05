372e4b59758efa8db57105a1f56050112b2e13e5
Gildeir <gildeirlopes@gmail.com>
2021-03-05 13:18:14 -0300

    Atualizando package

--- src/challenges2.js
+++ src/challenges2.js
@@ -27,6 +27,7 @@ function generatePhoneNumber(arr) {
   let store = 0;
   let count = 0;
   
+  
   if (check != 11) {
     return "Array com tamanho incorreto."
   }
@@ -40,11 +41,11 @@ function generatePhoneNumber(arr) {
   }
 
   //arr.sort()
-  for (indexA = 0; indexA < arr.length; indexA += 1) {    
-    for (indexB = indexA + 1; indexB < arr.length; indexB += 1) {      
+  for (let indexA = 0; indexA < arr.length; indexA += 1) {    
+    for (let indexB = indexA + 1; indexB < arr.length; indexB += 1) {      
       if (arr[indexA] == arr[indexB])
         count += 1; 
-        //console.log("Contou:" + count)       
+        console.log("Contou:" + count)       
     }
     if (count >= 3) {
       return "não é possível gerar um número de telefone com esses valores"
@@ -59,7 +60,7 @@ function generatePhoneNumber(arr) {
 
   return mask;
 }
-//console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
